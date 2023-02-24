bcfe3a323b55d7fecc84c9f4b5cb88997103ff63
Nicolas Zamboni <nicolas.zamboni@hotmail.com>
2023-02-24 18:35:50 -0300

    starting main section

--- vite-project/src/components/header/header.tsx
+++ vite-project/src/components/header/header.tsx
@@ -1,21 +1,52 @@
-import { FaSearch, FaHeart, FaUserCircle } from "react-icons/fa";
+import { useState } from "react";
+import {
+  FaSearch,
+  FaHeart,
+  FaUserCircle,
+  FaBars,
+  FaRegWindowClose,
+} from "react-icons/fa";
 
 export function Header() {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="bg-[#7a2d1b] flex gap-4 items-center h-24 justify-around text-white">
-      <div className="w-14 text-center">
-        <p>Lucie Cherie</p>
+    <div className="bg-[#7a2c1b] flex lg:gap-4 items-center h-24 justify-around text-white border-b">
+      <div
+        className={`${
+          open ? "bg-[#c75840] h-max " : "h-0 opacity-0"
+        } fixed top-20 md:hidden flex flex-col text-lg gap-2 w-auto py-2 px-4 rounded-lg left-5 transition-all duration-500 z-50`}
+      >
+        <p>Jewelry</p>
+        <p className="border-t pt-2">Coleções</p>
+        <p className="border-t pt-2">Promoções</p>
+        <p className="border-t pt-2">Sobre nós</p>
       </div>
-      <div className="flex gap-5 justify-center">
+      {!open ? (
+        <FaBars
+          onClick={() => setOpen(!open)}
+          size={20}
+          className="md:hidden"
+        />
+      ) : (
+        <FaRegWindowClose
+          onClick={() => setOpen(!open)}
+          size={20}
+          className="md:hidden"
+        />
+      )}
+      <div className="md:w-14 text-center">
+        <p>LUCIE CHERIE</p>
+      </div>
+      <div className="hidden md:flex gap-5 justify-center">
         <p>Jewelry</p>
         <p>Coleções</p>
         <p>Promoções</p>
         <p>Sobre nós</p>
       </div>
       <div className="flex gap-8 justify-center">
-        <FaSearch size={20}/>
-        <FaHeart size={20}/>
-        <FaUserCircle size={20}/>
+        <FaSearch size={20} />
+        <FaHeart size={20} />
+        <FaUserCircle size={20} />
       </div>
     </div>
   );
---
+++ vite-project/src/components/main-section/main-section.tsx
@@ -0,0 +1,12 @@
+export function Main() {
+  return (
+    <div className="relative bg-[#7a2c1bc9] -z-20">
+        <img src="https://cdn.shopify.com/s/files/1/2418/4873/products/MAK_5279_1024x1024@2x.jpg?v=1635330117" alt="oi" className="relative -z-10 md:rounded-b-full md:w-96 md:left-40"/>
+        <img src="https://cdn.shopify.com/s/files/1/1008/4952/products/JWJSET273_3c2b2ecd-b23a-4c19-9432-6682303edfbb_400x.jpg?v=1627637621" alt="oi" className="hidden absolute md:block md:rounded-full md:w-64 left-[50%] top-[60%]"/>
+    <div className="flex flex-col items-center justify-center absolute top-0 h-full w-full">
+        <p className="text-4xl text-white font-bold pb-3">Lucie Collection</p>
+        <button className="bg-[#04c780] z-0 px-5 py-2 rounded-lg text-lg">Ver mais</button>
+    </div>
+    </div>
+  );
+}
